24b5ee92a349564d330d27117a1b07768ba185e8
Jesse Griffin <jag3773@gmail.com>
2017-08-22 08:10:13 -0400

    Fixed missing space after hyphen search

--- process.py
+++ process.py
@@ -127,7 +127,7 @@ def findNew(tw_dict, usfm_file, config, tw_review):
              ' {0}!'.format(word) in line or
              ' {0})'.format(word) in line or
              '({0}'.format(word) in line or
-             '-{0}'.format(word) in line or
+             '-{0} '.format(word) in line or
              ' {0}-'.format(word) in line or
              ' {0}?'.format(word) in line or
              line.endswith(' {0}'.format(word)) ):
